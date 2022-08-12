003e70ffe6a0306493bc79a43aefae33bcc9e1c5
SIIPOLSKA\osegeda <osegeda@sii.pl>
2022-08-12 14:10:15 +0200

    [G] PR fix

--- app/src/main/java/com/gholem/moneylab/features/authentication/AuthenticationFragment.kt
+++ app/src/main/java/com/gholem/moneylab/features/authentication/AuthenticationFragment.kt
@@ -1,8 +1,6 @@
 package com.gholem.moneylab.features.authentication
 
 import android.app.Activity
-import android.content.Intent
-import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
 import com.gholem.moneylab.arch.base.BaseFragment
@@ -15,6 +13,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 import timber.log.Timber.i
 
 
@@ -26,11 +25,25 @@ class AuthenticationFragment :
 
     lateinit var authenticationNavigation: AuthenticationNavigation
 
+    private val signInLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val signInTask: Task<GoogleSignInAccount> =
+                    GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                if (signInTask.isSuccessful) {
+                    Timber.i("Logged on account: ${signInTask.getResult(ApiException::class.java).email}")
+                    viewModel.goToDashboard ()
+                } else {
+                    Timber.i("Login failed: ${signInTask.exception?.message}")
+                }
+            }
+        }
+
+
     override fun constructViewBinding(): FragmentAuthenticationBinding =
         FragmentAuthenticationBinding.inflate(layoutInflater)
 
     override fun init(viewBinding: FragmentAuthenticationBinding) {
-        verifyLastSignedInAccount()
         viewBinding.GoogleSignInButton.setOnClickListener {
             signIn()
         }
@@ -46,66 +59,9 @@ class AuthenticationFragment :
         viewModel.navigation.observe(this, authenticationNavigation::navigate)
     }
 
-    var getUser = registerForActivityResult(
-        ActivityResultContracts.StartActivityForResult(),
-        ActivityResultCallback {
-            if (it.resultCode == Activity.RESULT_OK) {
-
-                val data: Intent? = it.data
-
-                val signInTask: Task<GoogleSignInAccount> =
-                    GoogleSignIn.getSignedInAccountFromIntent(data)
-                val account = signInTask.getResult(ApiException::class.java)
-                i("Accont : ${account.email}")
-
-                if (signInTask.isSuccessful) {
-                    viewModel.goToDashboard()
-                }
-            } else {
-                i("Accont Doesnot exist")
-            }
-        }
-    )
 
     private fun signIn() {
         val signInIntent = getGoogleSignInClient().signInIntent
-        getUser.launch(signInIntent)
-    }
-
-
-/*
-    private fun onActivityResult(requestCode: Int, result: ActivityResult) {
-        if (result.resultCode == Activity.RESULT_OK) {
-            val intent = result.data
-            when (requestCode) {
-                RC_SIGN_IN -> {
-                    v.launch(intent)
-                    i("RC_SIGN_IN: ${result.data}")
-                }
-            }
-        }
-    }*/
-/*
-    private fun signIn() {
-        val signInIntent = getGoogleSignInClient().signInIntent
-        //startActivityForResult(signInIntent, RC_SIGN_IN)
-        resultLauncher.launch(signInIntent)
-    }
-
-    var resultLauncher = registerForActivityResult(
-        ActivityResultContracts.StartActivityForResult()) { result ->
-        i("!!!!!!!!!!!!!!!! ${result.resultCode}")
-        if (result.resultCode == RC_SIGN_IN) {
-            // There are no request codes
-            val data: Intent? = result.data
-
-        }
-    }
-*/
-
-    fun verifyLastSignedInAccount() {
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestEmail()
-            .build()
+        signInLauncher.launch(signInIntent)
     }
 }
